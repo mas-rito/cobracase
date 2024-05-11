8debb38d416a34eded445d6255471d4fc7e50849
Rito Ramadhan <ritoramadhan8@gmail.com>
2024-05-11 20:20:10 +0700

    style(melocules): add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 
 import "./globals.css"
 
+import { Footer } from "@/components/molecules/footer"
 import { Navbar } from "@/components/molecules/navbar"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-slate-50`}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
--- src/components/molecules/navbar.tsx
+++ src/components/molecules/navbar.tsx
@@ -15,7 +15,10 @@ export const Navbar = async () => {
     <nav className="sticky inset-x-0 top-0 z-[100] h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <div className="container">
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-          <Link href="/" className="z-40 flex font-semibold">
+          <Link
+            href="/"
+            className="z-40 flex font-semibold md:text-lg lg:text-2xl"
+          >
             case<span className="text-green-600">cobra</span>
           </Link>
 
